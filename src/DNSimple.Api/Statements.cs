c1499edc31e5f906860e02ef39d81e2b28aa719c
Geoff Appleford <geoff@appleford.me>
2012-03-13 09:07:02 +0000

    Add support for extended attributes and statements

---
+++ src/DNSimple.Api/Statements.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using RestSharp;
+using RestSharp.Validation;
+
+namespace DNSimple
+{
+	public partial class DNSimpleRestClient
+	{
+		/// <summary>
+		/// Retrieve the statements for this account.
+		/// Makes a GET request to the Statements resource.
+		/// </summary>
+		public dynamic GetStatements() {
+			var request = new RestRequest {
+				Resource = "statements"
+			};
+
+			return Execute<dynamic>(request);
+		}
+	}
+}
